25353967ef1517024042e60416645d1882b2d458
Jay T <jaytula@gmail.com>
2021-03-29 12:19:58 -0700

    Lesson 39. Testing Elmeent is Not on Page: Start Popover Tests

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.tsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.tsx
@@ -27,9 +27,24 @@ describe("SummaryForm", () => {
     render(<SummaryForm />);
 
     // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
 
     // popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
 
     // popover disappears when we mouse out
+    userEvent.unhover(termsAndConditions);
+    const nullPopoverAgain = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopoverAgain).not.toBeInTheDocument();
   });
 });
