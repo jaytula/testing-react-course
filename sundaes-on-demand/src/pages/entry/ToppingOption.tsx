b68b1721e8339bf12706e763fa3e60896501a4ec
Jay T <jaytula@gmail.com>
2021-04-12 00:41:09 -0700

    63. React Code: Toppings Checkboxes

--- sundaes-on-demand/src/pages/entry/ToppingOption.tsx
+++ sundaes-on-demand/src/pages/entry/ToppingOption.tsx
@@ -1,3 +1,4 @@
+import { Form } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 
 interface Props {
@@ -6,14 +7,34 @@ interface Props {
   updateItemCount: (name: string, count: string) => void;
 }
 
-const ToppingOption: React.FC<Props> = ({ name, imagePath }) => {
+const ToppingOption: React.FC<Props> = ({
+  name,
+  imagePath,
+  updateItemCount,
+}) => {
+  const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    updateItemCount(name, event.target.checked ? "1" : "0");
+  };
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
         style={{ width: "75%" }}
-        src={`http://localhost:3030${imagePath}`}
+        src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form>
+        <Form.Group controlId={`toppings-${name}`}>
+          <Form.Check
+            type="checkbox"
+            defaultChecked={false}
+            onChange={onChangeHandler}
+            label={name}
+          ></Form.Check>
+        </Form.Group>
+      </Form>
     </Col>
   );
 };
