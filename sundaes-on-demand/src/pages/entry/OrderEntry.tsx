47042c4d7ea6939977c5f948450a74e8c2d6972f
Jay T <jaytula@gmail.com>
2021-04-26 22:40:15 -0700

    78. Disable Order Button if No Scoops Ordered

--- sundaes-on-demand/src/pages/entry/OrderEntry.tsx
+++ sundaes-on-demand/src/pages/entry/OrderEntry.tsx
@@ -4,15 +4,18 @@ import Options from "./Options";
 
 const OrderEntry: React.FC<{ nextPhase: () => void }> = ({ nextPhase }) => {
   const {
+    scoops,
     totals: { grandTotal },
   } = useOrderDetails();
+
+  const scoopCount = [...scoops.entries()].reduce((acc, curr) => acc + curr[1], 0)
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand Total: {formatCurrency(grandTotal)}</h2>
 
-      <button onClick={nextPhase}>Order Sundae</button>
+      <button onClick={nextPhase} disabled={scoopCount === 0}>Order Sundae</button>
     </div>
   );
 };
--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.tsx
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.tsx
@@ -3,6 +3,8 @@ import {
   screen,
   waitFor,
 } from "../../../test-utils/testing-library-utils";
+
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -23,4 +25,26 @@ test("handles error for scoops and topping routes", async () => {
     const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
   });
+});
+
+test.only("order sundae button is disabled if there are no scoops", async () => {
+  render(<OrderEntry nextPhase={jest.fn()} />);
+
+  await screen.findByText(/grand total:.*0.00/i);
+
+  const orderButton = await screen.findByRole("button", {
+    name: /order sundae/i,
+  });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: /vanilla/i,
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
